c14705a9ef7a5c43d84f1aecbdc4d8ffad020cc4
Umair <saboowalaumair1404@gmail.com>
2024-11-09 22:40:45 +0530

    completed

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+
+    button.addEventListener('click', function(){
+        body.style.backgroundColor = button.id;
+    });
+});
+
+
